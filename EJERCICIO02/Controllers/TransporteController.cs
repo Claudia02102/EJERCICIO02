65a008c7b2ca2959d5109f96d97f7a17aba0a988
cpa02 <cpa02@DESKTOP-001F323>
2023-04-04 04:40:33 -0500

    Agregar archivos de proyecto.

---
+++ EJERCICIO02/Controllers/TransporteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EJERCICIO02.Controllers
+{
+    public class TransporteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EJERCICIO02/wwwroot/js/procesar.js
@@ -0,0 +1,16 @@
+﻿var cant = document.getElementById("cantidad");
+var prec = document.getElementById("precio");
+
+function getProcesar()
+{
+    var Importe_Compra = (parseInt(cant.value) * parseFloat(prec.value)).toFixed(2);
+    var Descuento = (Importe_Compra * 0.07).toFixed(2);
+    var Importe_Pagar = Importe_Compra - Descuento;
+    var cant_choco =  3 * parseInt(cant.value);
+
+    document.getElementById("resultado").value = "Importe de compra: " + Importe_Compra + "\n" +
+                                                 "Importe de descuento: " + Descuento + "\n" +
+                                                 "Importe a pagar: " + Importe_Pagar + "\n" +
+                                                 "Chocolates obsequiados: " + cant_choco;
+    return false;
+}
